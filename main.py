7066fac37dfde04d6ab84d0244f3b24f9240338a
JoshuaDauber <91396460+JoshuaDauber@users.noreply.github.com>
2022-04-06 10:15:44 -0400

    test input created, main file reads it

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #This is where the main script will go
+
+l = []
+
+with open('testInput.py') as f:
+    for line in f:
+        l.append(line)
+
+print(l)
---
+++ testInput.py
@@ -0,0 +1,19 @@
+#Test input, basic script to test the program on
+
+x = 1
+y = 2
+
+if x < 3:
+    print("x is less than 3")
+
+if y > 3:
+    print("y is greater than 3")
+
+y += 2
+
+if y > 3:
+    print("y is greater than 3 now")
+
+while x < 10:
+    print("x is less than 10")
+    x += 1
